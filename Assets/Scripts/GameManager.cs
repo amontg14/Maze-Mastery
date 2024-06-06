ca7bd68a4256c35ea608892e4ac56e3aa3d41e86
Ben Bushey <bennetskis@gmail.com>
2024-06-06 15:09:27 -0700

    fixed main menu more

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,22 +5,23 @@ public class GameManager : MonoBehaviour
 {
     public static bool gameStarted = false;
     private GameTimer gameTimer;
-    private RotateCamera rotateCamera;
+    public GameObject mainMenu;  // Reference to the MainMenu panel
+    public GameObject gameplayUI;  // Reference to the GameplayUI
 
     void Start()
     {
         gameTimer = FindObjectOfType<GameTimer>();
-        rotateCamera = FindObjectOfType<RotateCamera>(); // Find the RotateCamera script
-        gameTimer.enabled = false; // Disable the timer at the start
+        gameplayUI.SetActive(false);  // Hide Gameplay UI at the start
+        gameTimer.enabled = false;  // Disable the timer at the start
     }
 
     public void StartGame()
     {
         gameStarted = true;
-        gameTimer.enabled = true; // Enable the timer
-        gameTimer.StartTimer(); // Start the timer
-        GameObject.Find("MainMenu").SetActive(false);
-        rotateCamera.enabled = false; // Disable the RotateCamera script
+        gameTimer.enabled = true;  // Enable the timer
+        gameTimer.StartTimer();  // Start the timer
+        mainMenu.SetActive(false);  // Deactivate the MainMenu panel
+        gameplayUI.SetActive(true);  // Show Gameplay UI
     }
 
     public void QuitGame()
